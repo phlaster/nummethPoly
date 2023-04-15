2638fcebdd66f42c5ae4ab403b355d7ef828e2f4
Alex <curjucklloyd@gmail.com>
2023-04-15 20:59:56 +0300

    Minor refinements: * Now each result is writtent to separate .csv file * fstream error goes to sterr * minor enhancements, typo corrections

--- Cpp/3-1/3-1.cpp
+++ Cpp/3-1/3-1.cpp
@@ -1,94 +1,94 @@
 #include <iostream>
 #include <cmath>
+#include <fstream>
 
 using namespace std;
-
 
 // Variant 23:
 double polynom(double x)
 {
-    return\
-            pow(x, 5) -\
-        3.2*pow(x, 3) +\
+    return  pow(x,5) -\
+        3.2*pow(x,3) +\
         9.5*pow(x,2) -\
         7.0*x -\
         7.5;
 }
 
+// Antiderivative of given polynom
 double F(double x)
 {
-    return\
-            pow(x,6)/6.0 -\
+    return  pow(x,6)/6.0 -\
         3.2*pow(x,4)/4.0 +\
         9.5*pow(x,3)/3.0 -\
         3.5*pow(x,2) -\
         7.5*x;
 }
 
+// Newton-Leibniz formula
 double integralPrescise(double a=-0.7, double b=1.6)
 {
-    return F(b) - F(a); // Newton-Leibniz formula
+    return F(b) - F(a);
 }
 
-
+//Numeric calculations within given segment
 double integral(int n, double a=-0.7, double b=1.6)
 {
     double I = 0.0;
     double left = a;
-    double h = (b-a)/n;
-    double right = a + h;
-    
+    double dx = (b-a)/n;
+    double right = a + dx;
     for(int i=1; i <= n; i++)
         {
-            I += h * (polynom(left) + polynom(right)) / 2.0;
+            I += dx * (polynom(left) + polynom(right)) / 2.0;
             left = right;
-            right += h;
+            right += dx;
         }
     return I;
 }
 
-
-
 void integralBase(int nSteps = 2000)
 {
-
     double I_presc = integralPrescise();
-    double result;
-    for(int n = 1; n<=nSteps; n++)
-    {
-        result = abs(I_presc - integral(n));
-        cout << result << endl;
+    ofstream outfile("baseResult.csv");
+    if (outfile.is_open()) {
+        for(int n = 1; n<=nSteps; n++)
+        {
+            outfile << abs(I_presc-integral(n)) << endl;
+        }
+        outfile.close();
+        cout << "baseResult.csv has been written!" << endl;
     }
+    else cerr << "Couldn't write to file baseResult.csv!" << endl;
 }
 
 
 void integralRunge(int lowestPower = -10)
 {
-    double ETA = 1/3.0;
-    double eps;
-    double delta = 1.0;
-    
-    for(int eps_power = -1; eps_power > lowestPower; eps_power--)
-    {
-        delta = 1.0;
-        int n = 1;
-        eps = pow(10, eps_power);
-        while(delta > eps)
+    double ETA = 1/3.0; // Method constant for trapez method
+    ofstream outfile("rungeResult.csv");
+    if (outfile.is_open()) {
+        for(int eps_power = -1; eps_power >= lowestPower; eps_power--)
+        {
+            double delta = 1.0; // Initial *big* error
+            int n = 1;
+            double eps = pow(10, eps_power); //higher prescision at each iteration
+            while(delta > eps) // Runge method
             {        
                 delta = ETA*abs(integral(2*n) - integral(n));
                 n *= 2;
             }
-        cout << n << ", " << delta << ", " << eps << endl;
+            outfile << n/2 << "," << delta << "," << eps << endl;
+        }
+        outfile.close();
+        cout << "rungeResult.csv has been written!" << endl;
     }
+    else cerr << "Couldn't write to file rungeResult.csv!" << endl;
 }
 
-int main()
+int main(void)
 {
-    // float N;
-    // cout << "Enter number of steps:\n";
-    // cin >> N;
+
     integralBase();
     integralRunge();
-    // cout << "Prescise value = " << integralPrescise() << endl;
-    // cout << "Integral = " << I << endl;
+    return 0;
 }
