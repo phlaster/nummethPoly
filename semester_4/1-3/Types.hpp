a30bfeea0b1f5d9bedbeb4fd3622ac1632920d40
Alex <curjucklloyd@gmail.com>
2023-06-13 18:54:31 +0300

    removed comments

---
+++ semester_4/1-3/Types.hpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+typedef std::vector<double> Vector;
+#endif
+
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+typedef std::vector<Vector> Matrix;
+#endif
