974410e2b6bcd8a6abc3eed69003f0a9b842b597
Alex <curjucklloyd@gmail.com>
2023-10-23 14:39:50 +0300

    added make instruction for test

--- semester_3/0-4/test.cpp
+++ semester_3/0-4/test.cpp
@@ -3,13 +3,15 @@
 
 int main(){
     Mtr A = {
-        {1,2,3},
-        {4,5,6},
-        {7,8,9}
+        {2,2,2},
+        {2,3,3},
+        {2,3,3}
     };
     vInt perm = {0,1,2};
     print(A);
     find_max_and_swap(A, perm, 0);
+    print(A);
+    subtract_product(A, 0);
     print(A);
     print(perm);
 
