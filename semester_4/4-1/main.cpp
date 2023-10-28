f96fdf066248add47713fa8102bc4e775c076305
Alex <curjucklloyd@gmail.com>
2023-10-28 23:47:21 +0300

    messing with newlines

--- semester_4/4-1/main.cpp
+++ semester_4/4-1/main.cpp
@@ -34,30 +34,27 @@ double y_next(double y_i, double h_i, double f){
     return y_i + h_i * f;
 }
 
-void calculate(double a, double b, double h_i)
-{
+void calculate(double a, double b, double h_i){
     double y_0 = y(a);
     double h = b-a;
     double x_i = a;
     int n_steps = round(h/h_i);
 
     cout << "x_i,y_appr_i,y_i,err\n";
-    for (int i = 0; i < n_steps; i++)
-    {
+    for (int i = 0; i < n_steps; i++){
         double y_wave = y_wave_next(y_0, h_i, dy_dx(x_i, y_0));
         double y_i = y_next(y_0, h_i, dy_dx(x_i + h_i/2, y_wave));
         y_0 = y_i;
         x_i = a + i * h_i;
 
         cout << fixed << setprecision(6);
-        cout << x_i << ',' << y_i << ',' << y(x_i) << "," << abs (y_i - y(x_i)) << '\n';
+        cout << x_i << ',' << y_i << ',' << y(x_i) << "," << fabs (y_i - y(x_i)) << '\n';
 
     }
 }
 
 
-void calculate1_8(double a, double b)
-{
+void calculate1_8(double a, double b){
     double h = b-a;
     double x_i = a;
     cout << "h_i,err\n";
@@ -79,8 +76,7 @@ void calculate1_8(double a, double b)
     }
 }
 
-int main()
-{
+int main(){
     double a = 1.1, b = 3.0;
     
     // cout << "h_i=0.1\n";
--- semester_4/4-1/tex/listings/1.cpp
+++ semester_4/4-1/tex/listings/1.cpp
@@ -14,23 +14,21 @@ double y_next(double y_i, double h_i, double f){
     return y_i + h_i * f;
 }
 
-void calculate(double a, double b, double h_i)
-{
+void calculate(double a, double b, double h_i){
     double y_0 = y(a);
     double h = b-a;
     double x_i = a;
     int n_steps = round(h/h_i);
 
     cout << "x_i,y_appr_i,y_i,err\n";
-    for (int i = 0; i < n_steps; i++)
-    {
+    for (int i = 0; i < n_steps; i++){
         double y_wave = y_wave_next(y_0, h_i, dy_dx(x_i, y_0));
         double y_i = y_next(y_0, h_i, dy_dx(x_i + h_i/2, y_wave));
         y_0 = y_i;
         x_i = a + i * h_i;
 
         cout << fixed << setprecision(6);
-        cout << x_i << ',' << y_i << ',' << y(x_i) << "," << abs (y_i - y(x_i)) << '\n';
+        cout << x_i << ',' << y_i << ',' << y(x_i) << "," << fabs (y_i - y(x_i)) << '\n';
 
     }
 }
