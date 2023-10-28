4200695e2c228bdaa25a509e197311f9d7c41d39
Alex <curjucklloyd@gmail.com>
2023-10-29 00:56:45 +0300

    formatting and beautifying

--- semester_4/4-1/dot-fish-2/routines.cpp
+++ semester_4/4-1/dot-fish-2/routines.cpp
@@ -8,26 +8,33 @@ void Baza(void){
     ofstream 
         stream("data_baza_a.csv", ofstream::trunc),
         stream2("data_baza_b.csv", ofstream::trunc);
+
     // .IA.
     cout << "calculating for h = 0.1 ...\n";
-    vector<point> numsolution = RK2(exact_y(a), a, b, 0.1,&dydx);
-    //
+    vector<point> numsolution = RK2(exact_y(a), a, b, 0.1, &dydx);
+    
+
     cout << "writing to file ...\n";
     stream << "x,y_h=0.1,y_exact,err_h=0.1\n";
     for(int i = 0; i < numsolution.size(); i++){
         stream << numsolution[i].x << "," << numsolution[i].y << "," << exact_y(numsolution[i].x) << "," << fabs(numsolution[i].y - exact_y(numsolution[i].x)) << "\n";
     }
     stream.close();
+
+
     // .IB.
     cout << "calculating for h = 0.05 ...\n";
-    numsolution = RK2(exact_y(a), a, b, 0.05,&dydx);
-    //
+    numsolution = RK2(exact_y(a), a, b, 0.05, &dydx);
+    
+
     cout << "writing to file ...\n";
     stream2 << "x,y_h=0.05,y_exact,err_h=0.05\n";
     for(int i = 0; i < numsolution.size(); i++){
         stream2 << numsolution[i].x << "," << numsolution[i].y << "," << exact_y(numsolution[i].x) << "," << fabs(numsolution[i].y - exact_y(numsolution[i].x)) << "\n";
     }
     stream2.close();
+
+
     cout << "-------------------\n";
     //Построить зависимость (No2) ошибки от шага. На график нанести линию h 2 (почему?)
     ofstream stream3("data_baza_c.csv",ofstream::trunc);
@@ -40,7 +47,8 @@ void Baza(void){
         stream3 << h << "," << h*h << "," << maxerr << "\n";
     }
     stream3.close();
-    //-----------------------------
+
+
     cout << "...all done!\n";
     return;
 }
@@ -49,6 +57,7 @@ void Minimum(void){
     cout << "executing \"Minimum\" part of the assigment\n"; 
     //Построить зависимость 3, 4
     ofstream stream3("data_min.csv",ofstream::trunc);
+
     stream3 << "p,N,max_err\n";
     for (int i = 1; i < 9; i++){
         double p = pow(10,-i);
@@ -71,7 +80,8 @@ void Minimum(void){
         
     }
     stream3.close();
-    //-----------------------------
+
+
     cout << "...all done!\n";
     return;
 }
@@ -80,6 +90,7 @@ void Dostatochno(void){
     cout << "executing \"Dostatochno\" part of the assigment\n"; 
     //Построить зависимость 3, 4
     ofstream stream3("data_dost.csv",ofstream::trunc);
+
     stream3 << "p,N,max_err\n";
     for (int i = 1; i < 9; i++){
         double p = pow(10,-i);
@@ -90,7 +101,8 @@ void Dostatochno(void){
         stream3 << p << "," << numsolution.size() << "," << maxerr << "\n";
     }
     stream3.close();
-    //-----------------------------
+    
+
     cout << "...all done!\n";
     return;
 }
--- semester_4/4-1/dot-fish-2/runge.cpp
+++ semester_4/4-1/dot-fish-2/runge.cpp
@@ -1,45 +1,60 @@
 #include "Runge.hpp"
 
-double RK2_maxerror(double y0, double a, double b, double h, double (*func)(double, double), double (*etalon)(double)){
-    double maxerr = 0;
+double RK2_maxerror(
+    double y0, double a, double b, double h, double (*dydx)(double, double), double (*y_exact)(double)
+){
+    double maxerr = 0.0;
     double x = a, y = y0;
-    const double hh = h*0.5;
+    const double hh = h * 0.5;
     while(x <= b){
-        double _y = y + hh*func(x,y);
-        y += h*func(x+hh, _y);
+        double _y = y + hh * dydx(x,y);
+        y += h * dydx(x+hh, _y);
         x += h;
-        double err = fabs(etalon(x) - y);
+        double err = fabs(y_exact(x) - y);
         maxerr = err > maxerr ? err : maxerr;
     }
     return maxerr;
 }
 
-vector<point> RK2(double y0, double a, double b, double h, double (*func)(double, double))
-/*returns a vector of x,y pair values calculated using modified Euler (middle-point) method 
- *h - integration step, func - right hand of the equation for the first derivitive dy/dx*/{
+
+/*
+    returns a vector of x,y pair values calculated using modified Euler (middle-point) method 
+    *h - integration step,
+    func - right hand of the equation for the first derivitive dy/dx
+*/
+vector<point> RK2(
+    double y0, double a, double b, double h, double (*dydx)(double, double)
+){
     vector<point> Y = {{a,y0}};
     double x = a, y = y0;
     const double hh = h*0.5;
     while(x <= b){
-        double _y = y + hh*func(x,y);
-        y += h*func(x+hh, _y);
+        double _y = y + hh * dydx(x,y);
+        y += h * dydx(x+hh, _y);
         x += h;
         Y.push_back({x,y});
     }
     return Y;
 }
 
-vector<point> RK2_adaptive(double y0, double a, double b, double h0, double p, double (*func)(double, double)){
+vector<point> RK2_adaptive(
+    double y0, double a, double b, double h0, double p, double (*dydx)(double, double)
+){
     vector<point> Y = {{a,y0}};
     double x = a, y = y0;
     double h = h0;
     while(x <= b){
         while(1){
-            double hhi = h*0.5, hhj = h*0.25;
-            double _yi = y + hhi*func(x,y), _yj = y + hhj*func(x,y);
-            double yi = y + h*func(x+hhi, _yi), yj = y + hhi*func(x+hhj, _yj);
-            _yj = yj + hhj*func(x,yj);
-            yj += hhi*func(x+hhj, _yj);
+            double hhi = h * 0.5;
+            double _yi = y + hhi * dydx(x,y);
+            double  yi = y + h   * dydx(x+hhi, _yi);
+
+            double hhj = h * 0.25;
+            double _yj = y + hhj * dydx(x,y);
+            double  yj = y + hhi * dydx(x+hhj, _yj);
+
+            _yj = yj + hhj*dydx(x,yj);
+            yj += hhi*dydx(x+hhj, _yj);
             //cout << "<" << fabs(yi - yj)/3.0 << ">\n";
             if(fabs(yi - yj)/3.0 < p){
                 y = yi;
