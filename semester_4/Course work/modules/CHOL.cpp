5e50a59e1a0bf187f8cf6940296bfd13b66f0d1e
Alex <curjucklloyd@gmail.com>
2023-11-17 19:25:28 +0300

    deleted commented old revisions

--- semester_4/Course work/modules/CHOL.cpp
+++ semester_4/Course work/modules/CHOL.cpp
@@ -1,55 +1,5 @@
 #include "headers/HEADER.hpp"
 
-// Complete Cholesky
-// spMtr chol(const spMtr& A) {
-//     int N = A.cols;
-//     spMtr L(N, N);
-//     for (int I = 0; I < N; I++) {
-//         double S = A.get(I, I);
-//         for (int IP = 0; IP < I; IP++) {
-//             S -= L.get(I, IP) * L.get(I, IP);
-//         }
-//         if (S<0.0){
-//             cerr << "sqrt of negative on a diagonal!" << endl;
-//             exit(1);
-//         }
-//         L.set(sqrt(S), I, I);
-//         for (int J = I + 1; J < N; J++) {
-//             S = A.get(J, I);
-//             for (int IP = 0; IP < I; IP++) {
-//                 S -= L.get(J, IP) * L.get(I, IP);
-//             }
-//             double toset = S / L.get(I, I);
-//             L.set(toset, J, I);
-//         }
-//     }
-//     return L;
-// }
-
-// Incomplete Cholesky
-// spMtr ichol(const spMtr& A, double theta) {
-//     int N = A.cols;
-//     spMtr L(N, N);
-//     for (int I = 0; I < N; I++) {
-//         double S = A.get(I, I);
-//         for (int IP = 0; IP < I; IP++) {
-//             S -= L.get(I, IP) * L.get(I, IP);
-//         }
-//         L.set(sqrt(S), I, I);
-//         for (int J = I + 1; J < N; J++) {
-//             S = A.get(J, I);
-//             if (S == 0.0) continue; /////
-//             for (int IP = 0; IP < I; IP++) {
-//                 S -= L.get(J, IP) * L.get(I, IP);
-//             }
-//             double toset = S / L.get(I, I);
-//             if (fabs(toset) < theta) continue;/////
-//             L.set(toset, J, I);
-//         }
-//     }
-//     return L;
-// }
-
 spMtr chol(const spMtr& A) {
     int N = A.cols;
     spMtr L(N, N);
