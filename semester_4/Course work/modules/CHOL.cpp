da06823f45b61a69b8a8901f82660c8bf3f453e7
Alex <curjucklloyd@gmail.com>
2023-11-17 19:24:49 +0300

    i agree with this cholesky (comments with older revisions)

--- semester_4/Course work/modules/CHOL.cpp
+++ semester_4/Course work/modules/CHOL.cpp
@@ -1,28 +1,56 @@
 #include "headers/HEADER.hpp"
 
-spMtr chol(const spMtr& M) {
-    int n = M.cols;
-    spMtr L(n, n);
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j <= i; ++j) {
-            if (i == j) {
-                double sum = 0.0;
-                for (int k = 0; k < j; ++k) {
-                    sum += pow(L.get(j,k), 2);
-                }
-                L.set(sqrt(M.get(j,j) - sum), j, j);
-            } else {
-                double sum = 0.0;
-                for (int k = 0; k < j; ++k) {
-                    sum += L.get(i,k) * L.get(j,k);
-                }
-                L.set((M.get(i,j) - sum) / L.get(j,j), i,j);
-            }
-        }
-    }
-    return L;
-}
-spMtr ichol(const spMtr& A, double theta) {
+// Complete Cholesky
+// spMtr chol(const spMtr& A) {
+//     int N = A.cols;
+//     spMtr L(N, N);
+//     for (int I = 0; I < N; I++) {
+//         double S = A.get(I, I);
+//         for (int IP = 0; IP < I; IP++) {
+//             S -= L.get(I, IP) * L.get(I, IP);
+//         }
+//         if (S<0.0){
+//             cerr << "sqrt of negative on a diagonal!" << endl;
+//             exit(1);
+//         }
+//         L.set(sqrt(S), I, I);
+//         for (int J = I + 1; J < N; J++) {
+//             S = A.get(J, I);
+//             for (int IP = 0; IP < I; IP++) {
+//                 S -= L.get(J, IP) * L.get(I, IP);
+//             }
+//             double toset = S / L.get(I, I);
+//             L.set(toset, J, I);
+//         }
+//     }
+//     return L;
+// }
+
+// Incomplete Cholesky
+// spMtr ichol(const spMtr& A, double theta) {
+//     int N = A.cols;
+//     spMtr L(N, N);
+//     for (int I = 0; I < N; I++) {
+//         double S = A.get(I, I);
+//         for (int IP = 0; IP < I; IP++) {
+//             S -= L.get(I, IP) * L.get(I, IP);
+//         }
+//         L.set(sqrt(S), I, I);
+//         for (int J = I + 1; J < N; J++) {
+//             S = A.get(J, I);
+//             if (S == 0.0) continue; /////
+//             for (int IP = 0; IP < I; IP++) {
+//                 S -= L.get(J, IP) * L.get(I, IP);
+//             }
+//             double toset = S / L.get(I, I);
+//             if (fabs(toset) < theta) continue;/////
+//             L.set(toset, J, I);
+//         }
+//     }
+//     return L;
+// }
+
+spMtr chol(const spMtr& A) {
     int N = A.cols;
     spMtr L(N, N);
 
@@ -41,11 +69,40 @@ spMtr ichol(const spMtr& A, double theta) {
             for (int IP = 0; IP < I; IP++) {
                 S -= L.get(J, IP) * L.get(I, IP);
             }
-            if (fabs(S) > theta){
-                L.set(S / L.get(I, I), J, I);
-            }
+
+            L.set(S / L.get(I, I), J, I);
         }
     }
+    return L;
+}
 
+
+spMtr ichol(const spMtr& A, double theta) {
+    int N = A.cols;
+    spMtr L(N, N);
+
+    for (int I = 0; I < N; I++) {
+        double S = A.get(I, I);
+
+        for (int IP = 0; IP < I; IP++) {
+            S -= L.get(I, IP) * L.get(I, IP);
+        }
+
+        L.set(sqrt(S), I, I);
+
+        for (int J = I + 1; J < N; J++) {
+            S = A.get(J, I);
+            if (S==0.0) continue;
+
+            for (int IP = 0; IP < I; IP++) {
+                S -= L.get(J, IP) * L.get(I, IP);
+            }
+
+            double toset = S / L.get(I, I);
+            if (fabs(toset) <= theta) continue;
+
+            L.set(toset, J, I);
+        }
+    }
     return L;
 }
